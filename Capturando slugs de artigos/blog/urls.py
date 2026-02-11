5c9d58a023316be780a656a82896eec2d692058a
mariamonalisamacedo <caang.2024119tads0026@aluno.ifpi.edu.br>
2026-02-11 13:40:48 -0300

    Delete Capturando slugs de artigos directory

--- Capturando slugs de artigos/blog/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('artigo/<slug:slug>/', views.artigo, name='artigo'),
-]
--- Capturando slugs de artigos/slugs/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def artigo(request, slug):
-    return HttpResponse("Título do artigo (slug): {slug.replace("-"," ").title()}")
