323f78f625e128180e9d55d7000ee15019feef33
mariamonalisamacedo <caang.2024119tads0026@aluno.ifpi.edu.br>
2026-02-11 13:42:07 -0300

    Delete Validação de datas em rotas directory

--- Validação de datas em rotas/data/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse, HttpResponseBadRequest
-
-def data(request, ano, mes):
-    if not (1 <= mes <= 12):
-        return HttpResponseBadRequest('Mês inválido. Deve estar entre 1 e 12.')
-    return HttpResponse(f'Ano: {ano}, Mês: {mes:02d}')
