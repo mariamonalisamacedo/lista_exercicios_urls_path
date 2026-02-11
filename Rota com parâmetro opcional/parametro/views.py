146d07844cb24253c7f19cbba921c40580c692ca
mariamonalisamacedo <caang.2024119tads0026@aluno.ifpi.edu.br>
2026-02-11 13:41:42 -0300

    Delete Rota com parâmetro opcional directory

--- Rota com parâmetro opcional/parametro/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def perfil(request, nome=None):
-    if nome:
-        return HttpResponse(f'Perfil de {nome}')
-    else:
-        return HttpResponse('Perfil de visitante')
